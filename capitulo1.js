a7f51bf0818080ef40c3bde8a990be3f43757f7e
Your Name <you@example.com>
2022-10-04 03:23:53 -0300

    finalizando 1 capitulo

---
+++ capitulo1.js
@@ -0,0 +1,6 @@
+// Interagir com a API REST usada no curso
+// Identificar vulnerabilidades
+// O que é uma função de hashing
+// As diferenças entre funções de hashing
+// O funcionamento da função de hashing bcrypt
+// Implementar a proteção das senhas no banco de dados usando o bcrypt
