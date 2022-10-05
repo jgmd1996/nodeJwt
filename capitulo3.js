51662df37f6e2662dba7b6eb85b05950cdf3d6ec
Your Name <you@example.com>
2022-10-05 16:22:49 -0300

    Finalizando 3 capitulo

---
+++ capitulo3.js
@@ -0,0 +1,5 @@
+// Como gerar JSON Web Tokens;
+// Como configurar uma estratégia de autenticação para tokens;
+// Como gerar senhas aleatórias;
+// Como guardar valores em uma variável de ambiente;
+// Como tratar erros de autenticação.
